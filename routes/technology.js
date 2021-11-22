251129f2398b1c1cf4bebd756d0c301c736d3b72
dhana1701 <89499137+dhana1701@users.noreply.github.com>
2021-11-21 19:40:29 -0600

    added protection to update view

--- routes/technology.js
+++ routes/technology.js
@@ -8,11 +8,22 @@ router.get('/', technology_controlers.technology_view_all_Page);
 /* GET detail technology page */ 
 router.get('/detail', technology_controlers.technology_view_one_Page);
 
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
+
 /* GET create technology page */ 
 router.get('/create', technology_controlers.technology_create_Page); 
 
 /* GET create technology page */ 
-router.get('/update', technology_controlers.technology_update_Page);
+router.get('/update',secured, technology_controlers.technology_update_Page);
 
 /* GET create technology page */ 
 router.get('/delete', technology_controlers.technology_delete_Page);
